a499bed9b5f94b3baeef634af639e515980a4a2d
Matthew Bartl <mattbartl@gmail.com>
2019-03-20 15:07:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,7 @@
 def reverse_each_word(sentence)
-  sentence.each do 
-    split(' ')
-  end
+  sentence split(' ')
   sentence.each do |word|
     word.reverse!
   end
-  sentence.each do 
-    sentence1.join(' ')
-  end
+  sentence.join(' ')
 end
